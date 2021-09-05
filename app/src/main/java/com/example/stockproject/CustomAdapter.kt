70eda5055257e0c93957aeffc1af66db1f6c55ff
ramanpreet0899 <preetraman905@gmail.com>
2021-09-05 12:50:31 -0400

    detail screen added

--- app/src/main/java/com/example/stockproject/CustomAdapter.kt
+++ app/src/main/java/com/example/stockproject/CustomAdapter.kt
@@ -1,6 +1,7 @@
 import android.annotation.*
 import android.view.*
 import android.widget.*
+import androidx.cardview.widget.*
 import androidx.recyclerview.widget.*
 import com.example.stockproject.*
 import com.example.stockproject.model.*
@@ -8,15 +9,13 @@ import com.example.stockproject.model.*
 class CustomAdapter(private val title: MutableList<String>,private val content : MutableList<Stock>) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
+    var onStockNavigation:  ((Stock, String) -> Unit)? = null
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val cardView :CardView = view.findViewById(R.id.ps_card_view)
         val titleText: TextView = view.findViewById(R.id.ps_title)
         val companyNameText :TextView= view.findViewById(R.id.ps_company_name)
         val stockValueText :TextView= view.findViewById(R.id.ps_company_stock)
-
-
-        init {
-            // Define click listener for the ViewHolder's View.
-        }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -32,6 +31,12 @@ class CustomAdapter(private val title: MutableList<String>,private val content :
         viewHolder.titleText.text = title[position]
         viewHolder.companyNameText.text = content[position].name
         viewHolder.stockValueText.text = "$${content[position].price}"
+
+        viewHolder.cardView.setOnClickListener {
+            onStockNavigation?.let{
+                it(content[position],title[position])
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/stockproject/StockDetail.kt
@@ -0,0 +1,28 @@
+package com.example.stockproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import com.example.stockproject.model.*
+
+class StockDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stock_detail)
+
+        val symbolTv : TextView = findViewById(R.id.detail_symbol_content)
+        val nameTv :TextView = findViewById(R.id.detail_name_content)
+        val currentPriceTv : TextView = findViewById(R.id.detail_current_price_content)
+        val dailyLowTv : TextView = findViewById(R.id.detail_daily_low_content)
+        val dailyHighTv :TextView = findViewById(R.id.detail_daily_high_content)
+
+        val title = intent.getStringExtra("title")
+        symbolTv.text = title
+
+        val stock = intent.getSerializableExtra("stock") as Stock
+        nameTv.text = stock.name
+        currentPriceTv.text = "$${stock.price}"
+        dailyHighTv.text = "$${stock.high}"
+        dailyLowTv.text = "$${stock.low}"
+    }
+}
