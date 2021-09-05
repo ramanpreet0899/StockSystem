f858473ed31d4619ca64be9e125a831aa9c78ef4
ramanpreet0899 <preetraman905@gmail.com>
2021-09-04 23:21:39 -0400

    second commit with basic ui

--- app/src/main/java/com/example/stockproject/PopularStocks.kt
+++ app/src/main/java/com/example/stockproject/PopularStocks.kt
@@ -3,9 +3,9 @@ package com.example.stockproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PopularStocks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.popular_stocks)
     }
 }
