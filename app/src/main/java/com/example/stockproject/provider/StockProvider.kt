81ecaae06eb12debf7681946281d9f28b56f335d
ramanpreet0899 <preetraman905@gmail.com>
2021-09-05 23:02:56 -0400

    hash commit

--- app/src/main/java/com/example/stockproject/provider/StockProvider.kt
+++ app/src/main/java/com/example/stockproject/provider/StockProvider.kt
@@ -1,13 +1,18 @@
 package com.example.stockproject.provider
 
+import okhttp3.*
+import okhttp3.logging.*
 import retrofit2.*
 import retrofit2.converter.gson.*
 
 const val BASE_URL = "https://71iztxw7wh.execute-api.us-east-1.amazonaws.com/"
 
-class StockProvider {
+class StockProvider{
+
+    private val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(OkHttpClient.Builder().addInterceptor(interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)).build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
